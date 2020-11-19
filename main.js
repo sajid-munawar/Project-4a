01c84a5f5ad72d6671f19d393a2be76c9fde3e6e
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-19 10:52:23 +0500

    added animation withoud react using just simple html, css and javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+var cloud=document.getElementById("cloud")
+var cloudAnimation =cloud.animate([
+    {transform:"translateX(100%)"}
+],{
+    duration:5000,
+    iterations:Infinity,
+    direction:"reverse"
+})
+var cloud=document.getElementById("palm")
+var cloudAnimation =cloud.animate([
+    {transform:"translateX(100%)"}
+],{
+    duration:10000,
+    iterations:Infinity,
+    direction:"reverse"
+})
+
+var bird=document.getElementById("bird")
+var birdAnimation=bird.animate([
+    {transform:"translateX(100%)"}
+],{
+    duration:5000,
+    iterations:Infinity,
+    direction:"normal"
+})
